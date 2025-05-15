c017580c6ce72acc3495af465cf4690274e6fbab
vikas wankhede <vikasw@marketamerica.com>
2025-05-15 19:14:12 +0530

    ACF plugin pushed.

---
+++ wp-content/themes/vitsolutions/inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * vitsolutions Theme Customizer
+ *
+ * @package vitsolutions
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function vitsolutions_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'vitsolutions_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'vitsolutions_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'vitsolutions_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function vitsolutions_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function vitsolutions_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function vitsolutions_customize_preview_js() {
+	wp_enqueue_script( 'vitsolutions-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'vitsolutions_customize_preview_js' );
